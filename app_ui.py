e923b71dd26350cabf284ea6de52ae70493314ae
Santiago Briones <santiago.briones.777@gmail.com>
2026-09-20 11:13:44 -0600

    Sync references with papers in response processing

--- app_ui.py
+++ app_ui.py
@@ -25,7 +25,7 @@ from citas_evidencia import (
     formatear_contexto_papers, detectar_citas_alucinadas, detectar_citas_fuera_de_rango,
     detectar_negacion_contradictoria, limpiar_negacion_contradictoria,
     verificar_consistencia_fisiologica, construir_lista_fuentes, construir_contexto_para_juez,
-    eliminar_referencias_no_citadas,
+    eliminar_referencias_no_citadas, sincronizar_referencias_con_papers,
     evaluar_factualidad, resumen_evidencia_citada,
 )
 from ui_helpers import agregar_papers_a_chat, construir_panel_fuentes, anexar_badge_factualidad
@@ -806,7 +806,11 @@ def main(page: ft.Page):
                         tipo="error",
                     ))
 
-                full_response_limpio = eliminar_referencias_no_citadas(full_response)
+                full_response_limpio = sincronizar_referencias_con_papers(
+                    full_response,
+                    papers_relevantes,
+                    idioma_var[0],
+                )
                 if full_response_limpio != full_response:
                     full_response = full_response_limpio
                     ai_markdown.value = full_response
